723f752912d33ef2d8c675c017ef1733f8e92abb
MATEKUNA <kuna.mate@parlament.hu>
2022-12-08 16:18:10 +0100

    dynamic css with form-control

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -30,10 +30,9 @@ const CourseInput = props => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
-        <label style={{ color: !isValidInput ? 'red' : 'black' }}>Course Goal</label>
-        <input style={{ borderColor: !isValidInput ? 'red' : 'black', backgroundColor: !isValidInput ? 'salmon' : 'transparent' }}
-			type="text" onChange={goalInputChangeHandler} />
+      <div className={`form-control ${!isValidInput ? 'invalid' : ''}`}>
+        <label>Course Goal</label>
+        <input type="text" onChange={goalInputChangeHandler} />
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
