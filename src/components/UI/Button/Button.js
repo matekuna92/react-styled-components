9feec8f82636f8e72c5ea9d65b17c742a91f3983
matekuna92 <kunamate66@gmail.com>
2022-12-11 20:36:02 +0100

    dynamic styles and media query with CSS modules

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,41 +1,42 @@
 import React from 'react';
 
-import './Button.css';
-import styled from 'styled-components';
+import styles from './Button.module.css';
+//import styled from 'styled-components';
 
-// button method returns a new Button component. styled package has methods for every HTML element
-const Button = styled.button`
-    width: 100%;
-	font: inherit;
-	padding: 0.5rem 1.5rem;
-	border: 1px solid #8b005d;
-	color: white;
-	background: #8b005d;
-	box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
-	cursor: pointer;
+//// button method returns a new Button component. styled package has methods for every HTML element
+//const Button = styled.button`
+//    width: 100%;
+//	font: inherit;
+//	padding: 0.5rem 1.5rem;
+//	border: 1px solid #8b005d;
+//	color: white;
+//	background: #8b005d;
+//	box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+//	cursor: pointer;
+//
+//    @media(min-width: 768px) {
+//        width: auto;
+//    }
+//
+//	&:focus {
+//  		outline: none;
+//	}
+//
+//	&:hover,
+//	&:active {
+//		background: #ac0e77;
+//		border-color: #ac0e77;
+//		box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+//	}
+//`;
 
-    @media(min-width: 768px) {
-        width: auto;
-    }
-	
-	&:focus {
-  		outline: none;
-	}
-
-	&:hover,
-	&:active {
-		background: #ac0e77;
-		border-color: #ac0e77;
-		box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
-	}
-`;
-
-/* const Button = props => {
+//  style object has all the properties defined in the CSS file, so styles.button will apply the styles defined on .button class
+ const Button = props => {
   return (
-    <button type={props.type} className="button" onClick={props.onClick}>
+    <button type={props.type} className={styles.button} onClick={props.onClick}>
       {props.children}
     </button>
   );
-}; */
+};
 
 export default Button;
